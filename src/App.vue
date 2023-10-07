d0065c07048e4ba9babe999beae63bf17ac5eb65
singworld <xiaoxinbian@gmail.com>
2023-10-08 00:01:05 +0800

    fix(模块4): :bug: title
    body
    
    closed #1

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You have many!" />
+      <HelloWorld msg="You did many!" />
     </div>
   </header>
 
